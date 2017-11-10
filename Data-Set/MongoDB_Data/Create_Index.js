00fbf9db3d3f8ffe7798af99a6cbb7528d8731ca
Ritesh Sangurmath <sangurmath33@gmail.com>
2017-11-10 13:05:48 -0700

    Update Create_Index.js

--- Data-Set/MongoDB_Data/Create_Index.js
+++ Data-Set/MongoDB_Data/Create_Index.js
@@ -1,1 +1,13 @@
+// This script is used to create the index (primary key) for each collections (tables)
 
+db.employees.createIndex({"emp_no":1})
+db.department.createIndex({"dept_no":1})
+db.dept_man.createIndex({"emp_no":1})
+db.dept_man.createIndex({"dept_no":1})
+db.dept_emp.createIndex({"emp_no":1})
+db.dept_emp.createIndex({"dept_no":1})
+db.titles.createIndex({"emp_no":1})
+db.titles.createIndex({"titles":1})
+db.titles.createIndex({"from_date":1})
+db.salaries.createIndex({"emp_no":1})
+db.salaries.createIndex({"from_date":1})
